4370d6d3112c4456d031269f02a1f0ac7e01dfc6
Weslley Krutzmann <weslley.arthur.krutzmann@outlook.com>
2022-09-20 23:11:05 -0300

    Adicionado o EntityFramework e criado Banco de Dados.

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasData(
+                    new Country { Id = 1, Name = "Brazil", ShortName = "BRA" },
+                    new Country { Id = 2, Name = "United States of America", ShortName = "USA" }
+                );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                    new Hotel { Id = 1, Name = "Copacabana Palace", Address = "Av. Atlântica, 1702 - Copacabana, Rio de Janeiro - RJ", Rating = 5.0, CountryId = 1 },
+                    new Hotel { Id = 2, Name = "Kaimana Beach Hotel", Address = "2863 Kalākaua Ave, Honolulu, HI", Rating = 4.9, CountryId = 2 }
+                );
+        }
+    }
+}
---
+++ HotelListing.API/Migrations/20220921020509_SeededCountriesAndHotels.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    public partial class SeededCountriesAndHotels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 1, "Brazil", "BRA" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 2, "United States of America", "USA" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 1L, "Av. Atlântica, 1702 - Copacabana, Rio de Janeiro - RJ", 1, "Copacabana Palace", 5.0 });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 2L, "2863 Kalākaua Ave, Honolulu, HI", 2, "Kaimana Beach Hotel", 4.9000000000000004 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
